9014c3c3de2317cbeaa740e809f7fec4dbf4d6a9
Veshraj Ghimire <54109630+V35HR4J@users.noreply.github.com>
2020-10-21 09:21:21 -0700

    Update problem4.c

--- programming/problem4.c
+++ programming/problem4.c
@@ -1,1 +1,40 @@
 // Write a program in C to allow users to enter an arithmetic expression and display the result!
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    int sum, sub, mult, mod;
+    float div;
+
+    /*
+     * Input two numbers from user
+     */
+    printf("Enter 1st number:");
+    scanf("%d", &num1);
+    printf("Enter 2nd number:")
+    scanf("%d", &num2)
+
+    /*
+     * Perform all arithmetic operations
+     */ 
+    sum = num1 + num2;
+    sub = num1 - num2;
+    mult = num1 * num2;
+    div = (float)num1 / num2;
+    mod = num1 % num2;
+
+    /*
+     * Print result of all arithmetic operations
+     */
+    printf("SUM = %d\n", sum);
+    printf("DIFFERENCE = %d\n", sub);
+    printf("PRODUCT = %d\n", mult);
+    printf("QUOTIENT = %f\n", div);
+    printf("MODULUS = %d", mod);
+
+    return 0; 
+}
