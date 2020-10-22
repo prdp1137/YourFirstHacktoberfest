eb71d4cdb68ded6e3de52e19a8c71012ccd72d77
zetadeceleste <zdceleste@gmail.com>
2020-10-22 17:03:54 -0300

    Solve Problem / Add myself

--- programming/problem4.rb
+++ programming/problem4.rb
@@ -1,1 +1,29 @@
 # Write a program in Ruby to allow users to enter an arithmetic expression and display the result!
+
+print 'Enter a number:'
+num1 = gets
+
+print 'Enter another number:'
+num2 = gets
+
+puts 'Choose an operation:'
+puts 'A) +'
+puts 'B) -'
+puts 'C) *'
+puts 'D) /'
+
+print 'Your choice:'
+choice = gets
+
+case choice
+when 'A'
+    puts "The result of your operation is #{num1.to_i+num2.to_i}"
+when 'B'
+    puts "The result of your operation is #{num1.to_i-num2.to_i}"
+when 'C'
+    puts "The result of your operation is #{num1.to_i*num2.to_i}"
+when 'D'
+    puts "The result of your operation is #{num1.to_i/num2.to_i}"
+else 
+    puts 'Please enter a valid choice'
+end
