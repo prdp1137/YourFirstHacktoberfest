3e6c1be3e2136ec42f81e5ee1a0efcc07ba781b6
Binit Ghimire <thebinitghimire@gmail.com>
2020-10-30 18:26:31 +0545

    Update problem3.sh

--- programming/problem3.sh
+++ programming/problem3.sh
@@ -1,4 +1,4 @@
-# Write a shell script to remove the word "Hello" from all .md files in current directory recursively!
+# Write a shell script to remove the string "Hello" from all .md files in current directory recursively!
 
 #!/bin/bash
 # Your Solution Here!
