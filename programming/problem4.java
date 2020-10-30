da5274bcec33e9cb37ae8a26c70da09d83383cb9
Samir <41807740+Samir-Belaroui@users.noreply.github.com>
2020-10-30 14:00:17 +0000

    Problem4.java has been solved by @Samir-Belaroui
    Problem4.java has been solved by @Samir-Belaroui

--- programming/problem4.java
+++ programming/problem4.java
@@ -1,1 +1,53 @@
 // Write a program in Java to allow users to enter an arithmetic expression and display the result!
+public class Problem4 {
+     
+      public static void main(String[] args) {
+       
+          double firstNum;      double secondNum;     double value;  char operator;     
+                   
+          TextIO.putln("Enter expressions");
+          TextIO.putln("using any of the operators +, -, *, /.");
+          TextIO.putln("To end, enter a 0.");
+          TextIO.putln();
+          
+          while (true) {
+              
+            
+              TextIO.put("? ");
+              firstNum = TextIO.getDouble();
+              if (firstNum == 0)
+                 break;
+              operator = TextIO.getChar();
+              secondNum = TextIO.getlnDouble();
+              
+              
+              switch (operator) {
+                  case '+':
+                     value = firstNum + secondNum;
+                     break;
+                  case '-':
+                     value = firstNum - secondNum;
+                     break;
+                  case '*':
+                     value = firstNum * secondNum;
+                     break;
+                  case '/':
+                     value = firstNum / secondNum;
+                     break;
+                  default:
+                     TextIO.putln("Unknown operator: " + operator);
+                     continue; 
+              } 
+              
+             
+              
+              TextIO.putln("Value is " + value);
+              TextIO.putln();
+                        
+          } 
+          
+          TextIO.putln("Good bye");
+       
+       }  
+
+    }  
