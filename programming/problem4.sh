d2f3d789b615ffce2862d284f2dcf1025aba6c3a
prasiddha47 <73754908+prasiddha47@users.noreply.github.com>
2020-10-31 23:50:15 +0545

    Update problem4.sh

--- programming/problem4.sh
+++ programming/problem4.sh
@@ -1,13 +1,11 @@
 # "MultiWHOIS"
 # Write a shell script to perform multiple WHOIS lookups!
 
-
 ## Solution:
 
 #!/bin/bash
-read -p "Enter domain names separated by space ' ' : " input
+read -p "Enter domain names separated by space ' ': " input
 for i in ${input[@]}
 do
 whois $i
 done
-
